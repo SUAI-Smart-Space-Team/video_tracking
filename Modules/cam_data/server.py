c624a9d3596befa4a941e4ff4092b9545bfdd28d
Anna Krylova <56092304+lilfruitbat@users.noreply.github.com>
2021-04-23 17:18:37 +0300

    Update server.py

--- Modules/cam_data/server.py
+++ Modules/cam_data/server.py
@@ -1,56 +1,54 @@
-import os
 import time
-import struct
-
-import redis
-import numpy as np
 import pyrealsense2 as rs
+import numpy as np
+import redis
+import json
+from turbojpeg import TurboJPEG
+import os
 
 # Cam settings
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+# config.enable_device_from_file('test.bag')
 align = rs.align(rs.stream.color)
 profile = pipeline.start(config)
 depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
-
-# Depth filters
-decimation = rs.decimation_filter()
-decimation.set_option(rs.option.filter_magnitude, 3)
-spatial = rs.spatial_filter()
-spatial.set_option(rs.option.filter_magnitude, 2)
-spatial.set_option(rs.option.filter_smooth_alpha, 1)
-spatial.set_option(rs.option.filter_smooth_delta, 20)
-temporal = rs.temporal_filter()
-hole_filling = rs.hole_filling_filter()
-depth_to_disparity = rs.disparity_transform(True)
-disparity_to_depth = rs.disparity_transform(False)
 
 # Redis
 r = redis.Redis(host=os.getenv('SMART_SPACE_TRACKING_REDIS_PORT_6379_TCP_ADDR'), port=6379, db=0)
 pipe = r.pipeline()
 p = r.pubsub()
-r.set('depth_scale', depth_scale)
+
+jpeg = TurboJPEG()
+
+rp = int(os.getenv('IS_RP'))
 
 last_time = 0
+DM = {}
+# test
+fps = 0
+data_size = 0
 
-#test
-fps=0
-data_size=0
 
+def compress_rgb(image):
+    """
+    This method compesses RGB frame by TurboJPEG
+    :param image: RGB frame
+    :return: compressed RGB frame
+    """
 
+    return jpeg.encode(image)
 
 
 def get_frameset():
-"""
-This method gets frameset from  Intel Real Sense camera.
-
-Exceptions:
-    If any errors occur during code execution,it will reconnect to Intel Real Sense camera.
-
-    :return: set of frames
-"""
+    """
+    This method gets frameset from  Intel Real Sense camera.
+    Exceptions:
+        If any errors occur during code execution,it will reconnect to Intel Real Sense camera.
+        :return: set of frames
+    """
 
     while True:
         try:
@@ -65,121 +63,103 @@ Exceptions:
 
 
 def get_color_and_depth_frames():
-"""
-This method stores next frameset for later processing and gets color frames. After that  it creates alignment primitive with color as its target stream and filters depth map frames
-
-    :return: data of color frames and depth map frames
-"""
+    """
+    This method stores next frameset for later processing and gets color frames. After that  it creates alignment primitive with color as its target stream and filters depth map frames
+        :return: data of color frames and depth map frames
+    """
 
     frameset = get_frameset()
+
     color_frame = frameset.get_color_frame()
     color = np.asanyarray(color_frame.get_data())
+
     frameset = rs.align(rs.stream.color).process(frameset)
-    aligned_depth_frame = frameset.get_depth_frame()
-    # aligned_depth_frame = depth_to_disparity.process(aligned_depth_frame)
-    # aligned_depth_frame = spatial.process(aligned_depth_frame)
-    # aligned_depth_frame = temporal.process(aligned_depth_frame)
-    # aligned_depth_frame = disparity_to_depth.process(aligned_depth_frame)
-    # aligned_depth_frame = hole_filling.process(aligned_depth_frame)
 
-    depth = np.asanyarray(aligned_depth_frame.get_data())
+    depth_frame = frameset.get_depth_frame()
+    depth = np.asanyarray(depth_frame.get_data())
 
-    # colorizer = rs.colorizer()
-    # colorized_depth = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data())
+    if rp == 1:
+        color = np.rot90(color, 2).astype(np.uint8)
+        depth = np.rot90(depth, 2)
 
-    return color, depth, None  # colorized_depth
+    # x 0.85248447 0.84908537
+    # y 1.03574879 0.96441948
 
-
-def fromRedis(r, name):
-    encoded = r.get(name)
-    if struct.unpack('>I', encoded[:4]) == 3:
-        h, w, c = struct.unpack('>III', encoded[4:16])
-        a = np.frombuffer(encoded[16:], dtype=np.uint8).reshape(h, w, c)
-    else:
-        h, w = struct.unpack('>II', encoded[4:12])
-        a = np.frombuffer(encoded[12:], dtype=np.uint16).reshape(h, w)
-    return a
-
-
-def toRedis(arr: np.ndarray, name: str, pipe_redis):
-"""
-This method sends an array of data with a key to Redis
-
-Args:
-    arr (np.ndarray): array of data
-    name (str): key
-    pipe_redis: Redis pipeline
-"""
-
-    if len(arr.shape) == 3:
-        h, w, c = arr.shape
-        shape = struct.pack('>IIII', 3, h, w, c)
-    else:
-        h, w = arr.shape
-        shape = struct.pack('>III', 2, h, w)
-    encoded = shape + arr.tobytes()
-
-    pipe_redis.set(name, encoded)
-    return
+    return color, depth, frameset.timestamp
 
 
 def send_data(msg):
-"""
-This method sends to Redis fps parameter, color frames and depth map frames and mapping between the units of the depth image and meters. After that it publishes that data was sent.
+    """
+    This method sends to Redis fps parameter, color frames and  mapping between the units of the depth image and meters on request.
+    Args:
+        msg: request key
+    """
+    fps = 0
+    data_size = 0
 
-Args:
-    msg:
-"""
+    if msg['type'] != 'message':
+        return
 
-    global last_time
-    pipe.set('fps', str(1 / (time.time() - last_time)))
-    # print(1 / (time.time() - last_time))
-    #print('FPS: {:.4f}'.format(1 / (time.time() - last_time)))
-    last_time = time.time()
+    data = json.loads(msg['data'].decode('ascii'))
 
-    # r.flushall()
+    if data['req'] == 'cf':
+        color_image, depth_image, timestamp = get_color_and_depth_frames()
+        timestamp = '{}'.format(timestamp)
+        DM[timestamp] = depth_image
+        image = compress_rgb(color_image)
 
-    color, depth, colorized_depth = get_color_and_depth_frames()
-    toRedis(color, 'frame_color', pipe)
-    toRedis(depth, 'frame_depth', pipe)
+        pipe.set('cf_' + timestamp, image)
+        pipe.publish('pipeline', json.dumps(dict(module='camera', ans='cf', ts=timestamp)))
 
-    # toRedis(colorized_depth, 'frame_colorized', pipe)
-    pipe.set('depth_scale', depth_scale)
+    elif data['req'] == 'df':
+        timestamp = data['ts']
+        points = np.frombuffer(r.get('detect-points_' + timestamp), dtype=np.uint16)
+        if points.size != 0:
+            points = points.reshape((-1, 18, 2))
+            distance = np.zeros(points.shape[0], dtype=np.float16)
+            depth_map = DM[timestamp]
+
+            for ind in range(distance.size):
+                distance[ind] = np.mean(depth_map[points[ind][:, 0], points[ind][:, 1]]) * depth_scale
+
+            DM.pop(timestamp)
+            pipe.set('distance_' + timestamp, distance.tobytes())
+        else:
+            pipe.set('distance_' + timestamp, '')
+
+        pipe.publish('pipeline', json.dumps(dict(module='camera', ans='df', ts=timestamp)))
 
     pipe.execute()
     fps = 1 / (time.time() - last_time)
-    data_size = data_size + len(color) + len(depth)
+    data_size = data_size + len(color_image) + len(depth_image)
     data_rate = data_size / (time.time() - last_time)
-    testFPS()
-    testDataRate()
-    # print('\rFPS: {}'.format(1 / (time.time() - start)), end='')
-    r.publish('detection-server', 'frame_color frame_depth frame_colorized depth_scale')
+    testFPS(fps)
+    testDataRate(data_rate)
 
 
-if __name__ == '__main__':
-    for i in range(60):
-        get_color_and_depth_frames()
+def testFPS(fps):
+    """
+    This method tests fps for transmission
 
-    p.subscribe(**{'cam-data-server': send_data})
-    thread = p.run_in_thread(sleep_time=0.00001)
-    send_data('test')
-    thread.join()
-
-
-def testFPS:
-"""
-This method tests fps for transmission 
-
-"""
+    """
 
     print('FPS =    '.format(fps))
     # toRedit(fps, 'FPS',pipe)
 
 
-def testDataRate
-"""
-This method tests data rate for transmission 
+def testDataRate(data_rate):
+    """
+    This method tests data rate for transmission
 
-"""
+    """
     print('Data rate =  '.format(data_rate))
     # toRedit(data_rate, 'DATA_RATE',pipe)
+
+    if __name__ == '__main__':
+        for i in range(60):
+            get_color_and_depth_frames()
+        r.set('im-shape', '720 1280')
+
+        p.subscribe(**{'cam-data-server': send_data})
+        thread = p.run_in_thread(sleep_time=0.00001)
+        thread.join()
